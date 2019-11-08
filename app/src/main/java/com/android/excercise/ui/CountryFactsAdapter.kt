d1b718b12dd03f00de485e9d06eb800e04fc4aa9
Rohan Wagh <rohan.wagh@cognizant.com>
2019-11-08 17:55:18 +0530

    Recyclerview Adapter implemented and UI changes implemented

--- app/src/main/java/com/android/excercise/ui/CountryFactsAdapter.kt
+++ app/src/main/java/com/android/excercise/ui/CountryFactsAdapter.kt
@@ -1,6 +1,48 @@
 package com.android.excercise.ui
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.excercise.R
+import com.android.excercise.data.model.CountryFacts
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.country_facts_item.view.*
 
-class CountryFactsAdapter {
+class CountryFactsAdapter:RecyclerView.Adapter<CountryFactsAdapter.CountryFactsViewHolder>() {
+
+    var countryFactList: List<CountryFacts>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryFactsViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.country_facts_item,parent,false)
+        return CountryFactsViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return countryFactList?.size ?:0
+    }
+
+    override fun onBindViewHolder(holder: CountryFactsViewHolder, position: Int) {
+        val countryFact = countryFactList?.get(position)
+        holder.bind(countryFact)
+    }
+
+
+    class CountryFactsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        fun bind(countryFacts: CountryFacts?){
+            itemView.apply {
+                title.text = countryFacts?.title
+                description.text = countryFacts?.description
+                countryFacts?.image?.let { imageurl ->
+                    Picasso.get()
+                        .load(imageurl)
+                        .fit().centerInside()
+                        .error(R.drawable.ic_launcher_background)
+                        .into(factImage)
+                }
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/android/excercise/ui/MainActivity.kt
+++ app/src/main/java/com/android/excercise/ui/MainActivity.kt
@@ -2,14 +2,25 @@ package com.android.excercise.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.excercise.R
+import com.android.excercise.data.DataState
 import com.android.excercise.databinding.ActivityMainBinding
+import com.android.excercise.di.DaggerCountryFactsComponent
 import com.android.excercise.viewmodel.CountryFactsViewModel
+import com.android.excercise.viewmodel.CountryFactsViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var countryFactsViewModelFactory: CountryFactsViewModelFactory
 
     lateinit var countryFactsViewModel:CountryFactsViewModel
 
@@ -18,13 +29,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        DaggerCountryFactsComponent.builder().build().inject(this)
+
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        countryFactsViewModel =
+            ViewModelProviders.of(this, countryFactsViewModelFactory).get(CountryFactsViewModel::class.java)
 
         binding.countryFactsViewModel = countryFactsViewModel
         binding.lifecycleOwner = this
 
         setupRecyclerView()
         setUpSwipeToRefreshLayout()
+
+        countryFactsViewModel.getCountryFactsDetails()
+
+        observeCountryFactsData()
     }
 
 
@@ -33,14 +53,34 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.apply {
             layoutManager = linearLayoutManager
-
+            countryFactsAdapter = CountryFactsAdapter()
+            adapter = countryFactsAdapter
         }
     }
 
 
     private fun setUpSwipeToRefreshLayout(){
         swipeRefresh.setOnRefreshListener{
-            // Need to call ViewModel
+            countryFactsViewModel.getCountryFactsDetails()
         }
+    }
+
+    private fun observeCountryFactsData(){
+        countryFactsViewModel.getCountryDataState()
+            ?.observe(this, Observer { factsDataStatus ->
+                when (factsDataStatus) {
+
+                    is DataState.Success -> {
+                        Log.d("XYZ",factsDataStatus.countryData?.title)
+                        countryFactsAdapter.countryFactList = factsDataStatus.countryData?.factList
+                        countryFactsAdapter.notifyDataSetChanged()
+                        swipeRefresh.isRefreshing = false
+                    }
+
+                    is DataState.Error -> {
+
+                    }
+                }
+            })
     }
 }
